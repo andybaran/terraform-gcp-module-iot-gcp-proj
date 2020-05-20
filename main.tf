13762c579a5839a0b2d7edd1f817530f2df48817
Andy Baran <andy.baran@hashicorp.com>
2020-05-20 16:18:13 -0400

    dont use billing account

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "project-factory_example_fabric_project" {
   source          = "terraform-google-modules/project-factory/google//modules/fabric-project"
   version = "8.0.1"
   activate_apis   = var.activate_apis
-  billing_account = var.billing_account
+  //billing_account = var.billing_account
   name            = var.name
   owners          = var.owners
   parent          = var.parent
