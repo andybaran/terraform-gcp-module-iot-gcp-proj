355af758f0784dfc95c9925a5211bb6bf940f953
Andy Baran <andy.baran@hashicorp.com>
2020-05-29 13:11:37 -0400

    lots of changes

--- main.tf
+++ main.tf
@@ -1,226 +1,93 @@
+
 terraform {
     required_version = ">= 0.12.0"
+    required_providers {
+        google = "3.5.0"
+        random = "~> 2.2"
+  }
 }
 
 provider "google" {
-  credentials = var.creds
+    region = var.region
+    credentials = creds
+    project = "andybaran-seedproject"
 }
 
-module "project-factory_example_fabric_project" {
-  source          = "terraform-google-modules/project-factory/google//modules/fabric-project"
-  version = "8.0.1"
-  activate_apis   = var.activate_apis
-  billing_account = var.billing_account
-  name            = var.name
-  owners          = var.owners
-  parent          = var.parent
-  prefix          = var.prefix
-  oslogin         = true
+resource "random_id" "id" {
+    byte_length = 4
+    prefix = var.project_name
 }
 
-module "project-iam-bindings" {
-  source          = "terraform-google-modules/iam/google//modules/projects_iam"
-  projects = ["${module.project-factory_example_fabric_project.project_id}"]
+resource "google_project" "project" {
+    name = var.project_name
+    project_id = random_id.id.hex
+    org_id = var.org_id
+    folder_id = var.folder_id
+    billing_account = var.billing_account
+    auto_create_network = false
+    
+    lifecycle {
+            prevent_destroy = true
+    }
+}
 
-  bindings = {
-    "roles/owner" = [
-      "user:andy.baran@hashicorp.com",
-    ]
+resource "google_project_service" "common_services" {
+    for_each = toset([
+        "compute.googleapis.com",
+        "logging.googleapis.com",
+        "monitoring.googleapis.com",
+        "storage-component.googleapis.com",
+    ])
+    
+    service = each.key
+    project =  data.google_project.project.project_id
+    disable_dependent_services = true
+}
+
+resource "google_project_service" "requested_services" {
+    for_each = toset([${requested_services}])
+    
+    service = each.key
+    project =  data.google_project.project.project_id
+    disable_dependent_services = true
+}
+
+
+# We need compute engine api enabled before we can create networks
+resource "google_compute_network" "provisioning-vpc" {
+
+  depends_on = [google_project_service.common_services]
+  
+  name = "provisioning-vpc"
+  project = data.google_project.project.project_id
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "provisioning-subnet" {
+  name          = join(google_project.project,"-primary-subnet")
+  ip_cidr_range = "10.10.0.0/16"
+  project       = data.google_project.project.project_id
+  region        = var.region
+  network       = google_compute_network.provisioning-vpc.self_link
+  secondary_ip_range {
+    range_name    = join(google_project.project,"-secondary-range")
+    ip_cidr_range = "192.168.10.0/24"
   }
 }
 
-
-resource "random_string" "prefix" {
-  length  = 30 - length(var.name) - 1
-  upper   = false
-  number  = false
-  special = false
+resource "google_service_account" "admin_service_account" {
+  account_id   = "admin-gcpkms"
+  display_name = "Admin service account for GCP"
+  project =  google_project.project.project_id
+  depends_on = [google_project.project]
 }
 
-// ************************************************************   
-// IoT Core
-// ************************************************************  
+resource "google_project_iam_binding" {
+    project = "google_project.project.project_id"
+    role = "roles/owner"
 
-resource "google_cloudiot_registry" "iot_registry" { 
-    depends_on = [google_pubsub_topic.pst_diagnostic_data]
-    
-    name = "obd2_devices"
-    project = module.project-factory_example_fabric_project.project_id
-    event_notification_configs {
-        pubsub_topic_name = "projects/${module.project-factory_example_fabric_project.project_id}/topics/diagnostic_data"
-    }
-    mqtt_config = {
-        mqtt_enabled_state = "MQTT_ENABLED"
-    }
-    http_config = {
-        http_enabled_state = "HTTP_ENABLED"
-    }
-}
-
-// ************************************************************   
-// Cloud Pub Sub
-// ************************************************************  
-
-resource "google_pubsub_topic" "pst_diagnostic_data" {
-    name = "diagnostic_data"
-    project = module.project-factory_example_fabric_project.project_id
-}
-
-resource "google_pubsub_subscription" "pst_diagnostic_data_sub" {
-    depends_on = [google_pubsub_topic.pst_diagnostic_data]
-    name = var.pub_sub_sub
-    project = module.project-factory_example_fabric_project.project_id
-    topic = google_pubsub_topic.pst_diagnostic_data.name
-    
-    message_retention_duration = "86400s"
-    retain_acked_messages = true
-}
-
-// ************************************************************   
-// BigQuery Dataset & Table
-// ************************************************************   
-
-resource "google_bigquery_dataset" "obd2info" {
-    project = module.project-factory_example_fabric_project.project_id
-    dataset_id = var.bq_dataset
-    friendly_name = var.bq_dataset
-    description = "Dataset containing tables related to OBD2 diagnostic logs"
-    location = "US"
-
-   /* access {
-        role = "projects/${module.project-factory_example_fabric_project.project_id}/roles/bigquery.admin"
-        special_group = "projectOwners"
-    }
-    access {
-        role = "projects/${module.project-factory_example_fabric_project.project_id}/roles/bigquery.dataEditor"
-        special_group = "projectWriters"
-    }
-    access {
-        role = "projects/${module.project-factory_example_fabric_project.project_id}/roles/bigquery.dataViewer"
-        special_group = "projectReaders"
-    }
-    access {
-        role = "projects/${module.project-factory_example_fabric_project.project_id}/roles/bigquery.jobUser"
-        special_group = "projectWriters"
-    }
-    access {
-        role = "projects/${module.project-factory_example_fabric_project.project_id}/bigquery.jobUser"
-        special_group = "projectReaders"
-    }*/
-}
-
-resource "google_bigquery_table" "obd2logging" {
-    project = module.project-factory_example_fabric_project.project_id
-    dataset_id = google_bigquery_dataset.obd2info.dataset_id
-    table_id = var.bq_table
-
-    schema = <<EOF
-    [
-    {
-        "mode": "NULLABLE", 
-        "name": "VIN", 
-        "type": "STRING"
-      }, 
-      {
-        "mode": "NULLABLE", 
-        "name": "collectedAt", 
-        "type": "STRING"
-      }, 
-      {
-        "mode": "NULLABLE", 
-        "name": "PID_RPM", 
-        "type": "FLOAT"
-      }, 
-      {
-        "mode": "NULLABLE", 
-        "name": "PID_ENGINE_LOAD", 
-        "type": "FLOAT"
-      }, 
-      {
-        "mode": "NULLABLE", 
-        "name": "PID_COOLANT_TEMP", 
-        "type": "FLOAT"
-      }, 
-      {
-        "mode": "NULLABLE", 
-        "name": "PID_ABSOLUTE_ENGINE_LOAD", 
-        "type": "FLOAT"
-      }, 
-      {
-        "mode": "NULLABLE", 
-        "name": "PID_TIMING_ADVANCE", 
-        "type": "FLOAT"
-      }, 
-      {
-        "mode": "NULLABLE", 
-        "name": "PID_ENGINE_OIL_TEMP", 
-        "type": "FLOAT"
-      }, 
-      {
-        "mode": "NULLABLE", 
-        "name": "PID_ENGINE_TORQUE_PERCENTAGE", 
-        "type": "FLOAT"
-      }, 
-      {
-        "mode": "NULLABLE",
-        "name": "PID_ENGINE_REF_TORQUE", 
-        "type": "FLOAT"
-      }, 
-      {
-        "mode": "NULLABLE",   
-        "name": "PID_INTAKE_TEMP", 
-        "type": "FLOAT"
-      },
-      {
-        "mode": "NULLABLE",   
-        "name": "PID_MAF_FLOW", 
-        "type": "FLOAT"
-      },
-      {
-        "mode": "NULLABLE", 
-        "name": "PID_BAROMETRIC", 
-        "type": "FLOAT"
-      }, 
-      {
-        "mode": "NULLABLE",  
-        "name": "PID_SPEED", 
-        "type": "FLOAT"
-      }, 
-      {
-        "mode": "NULLABLE",   
-        "name": "PID_RUNTIME", 
-        "type": "FLOAT"
-      },
-      {
-        "mode": "NULLABLE",   
-        "name": "PID_DISTANCE", 
-        "type": "FLOAT"
-      }
+    members = [
+      "user: andy.baran@hashicorp.com",
+      "user: google_service_account.admin_service_account.account_id"
     ]
-    EOF
-
-
-}
-
-// ************************************************************   
-// Dataflow Job (PubSub --> BigQuery Table)
-// ************************************************************   
-
-resource "google_storage_bucket" "dataflow_bucket" {
-  project = module.project-factory_example_fabric_project.project_id
-  name = join("",["dataflow-", module.project-factory_example_fabric_project.project_id])
-  location = "US"
-}
-
-resource "google_dataflow_job" "collect_OBD2_data" {
-  project = module.project-factory_example_fabric_project.project_id
-  name              = "OBD2-Data-Collection"
-  zone = var.zone
-  template_gcs_path = "gs://dataflow-templates/latest/PubSub_Subscription_to_BigQuery"
-  temp_gcs_location = "${google_storage_bucket.dataflow_bucket.url}/tmp_dir"
-  parameters = {
-    inputSubscription = "projects/${module.project-factory_example_fabric_project.project_id}/subscriptions/${var.pub_sub_sub}"
-    outputTableSpec = "${module.project-factory_example_fabric_project.project_id}:${var.bq_table}"
-    #flexRSGoal = "COST_OPTIMIZED"
-  }
 }
--- variables.tf
+++ variables.tf
@@ -14,13 +14,8 @@ variable "zone" {
     description = "GCP Zone"
 }
 
-variable "parent" {
-  description = "The resource name of the parent Folder or Organization. Must be of the form folders/folder_id or organizations/org_id."
-  type        = string
-}
-
-variable "prefix" {
-  description = "Prefix used to generate project id and name."
+variable "folder_id" {
+  description = "The resource name of the parent Folder . Must be of the form google_folder.folder_id"
   type        = string
 }
 
@@ -38,102 +33,14 @@ variable "billing_account" {
 variable "activate_apis" {
   description = "Service APIs to enable."
   type        = list(string)
-  default     = []
+variable "org_id" {
+  description = "GCP Organization id."
+  type = string 
+  default = ""
 }
 
-variable "owners" {
-  description = "Optional list of IAM-format members to set as project owners."
-  type        = list(string)
-  default     = []
-}
-
-variable "editors" {
-  description = "Optional list of IAM-format members to set as project editor."
-  type        = list(string)
-  default     = []
-}
-
-variable "viewers" {
-  description = "Optional list of IAM-format members to set as project viewers."
-  type        = list(string)
-  default     = []
-}
-
-variable "lien_reason" {
-  description = "If non-empty, creates a project lien with this description."
-  type        = string
-  default     = ""
-}
-
-variable "oslogin" {
-  description = "Enable oslogin."
-  type        = bool
-  default     = false
-}
-
-variable "oslogin_admins" {
-  description = "List of IAM-format members that will get OS Login admin role."
-  type        = list(string)
-  default     = []
-}
-
-variable "oslogin_users" {
-  description = "List of IAM-format members that will get OS Login user role."
-  type        = list(string)
-  default     = []
-}
-
-# TODO: revert to a single map once the following issue is fixed
-# https://github.com/hashicorp/terraform/issues/12570
-
-variable "extra_bindings_roles" {
-  description = "List of roles for additional IAM bindings, pair with members list below."
-  type        = list(string)
-  default     = []
-}
-
-variable "extra_bindings_members" {
-  description = "List of comma-delimited IAM-format members for additional IAM bindings, one item per role."
-  type        = list(string)
-  default     = []
-}
-
-variable "auto_create_network" {
-  description = "Whether to create the default network for the project"
-  type        = bool
-  default     = false
-}
-
-variable "custom_roles" {
-  description = "Map of role name => comma-delimited list of permissions to create in this project."
-  type        = map(string)
-  default     = {}
-}
-
-variable "gce_service_account_roles" {
-  description = "List of project id=>role to assign to the default GCE service account."
-  type        = list(string)
-  default     = []
-}
-
-variable "labels" {
-  description = "Resource labels."
-  type        = map(string)
-  default     = {}
-}
-
-variable "pub_sub_sub" {
-  description = "Pub/Sub Subscription"
-  type = string
-  
-}
-
-variable "bq_dataset" {
-  description = "BigQuery Dataset"
-  type = string
-}
-
-variable "bq_table" {
-  description = "BigQuery Table"
-  type = string
+variable requested_services {
+  description = "Additional APIs to enable for your project"
+  type = list(string)
+  default = [""]
 }
