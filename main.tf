de5f52f55e7b56cc41d4e6e6148f0a1af4fa66a7
Andy Baran <andy.baran@hashicorp.com>
2020-06-02 13:20:58 -0400

    member only

--- main.tf
+++ main.tf
@@ -84,11 +84,11 @@ resource "google_service_account" "admin_service_account" {
 resource "google_project_iam_member" "proj_owners_serviceAccount" {
     project = google_project.project.id
     role = "roles/owner"
-    members = "serviceAccount:google_service_account.admin_service_account.email"
+    member = "serviceAccount:google_service_account.admin_service_account.email"
 }
 
 resource "google_project_iam_member" "proj_owners_adminUser" {
     project = google_project.project.id
     role = "roles/owner"
-    members = "user:andy.baran@hashicorp.com"
+    member = "user:andy.baran@hashicorp.com"
 }
