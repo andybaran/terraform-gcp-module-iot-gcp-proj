d06bbe2fe19a0167dbebe905759f20bf0e6a0c23
Andy Baran <andy.baran@hashicorp.com>
2020-06-02 12:57:21 -0400

    fixed line 10

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,7 @@ output "project_id" {
 }
 
 output "short_project_id" {
-  value = value = google_project.project.short_project_id
+  value = google_project.project.short_project_id
 }
 
 output "service_account" {
