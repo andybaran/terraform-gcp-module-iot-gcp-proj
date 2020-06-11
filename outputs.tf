cc564a1f0c902aa261c9279b3896d8d986b6e5ee
Andy Baran <andy.baran@hashicorp.com>
2020-06-11 17:26:37 -0400

    removed dupe output

--- outputs.tf
+++ outputs.tf
@@ -18,10 +18,6 @@ output "service_account_token" {
   value = google_service_account_key.sa_token.private_key
 }
 
-output "service_account" {
-  value = google_service_account.admin_service_account.account_id
-}
-
 output "service_account_email" {
   value = google_service_account.admin_service_account.email
 }
